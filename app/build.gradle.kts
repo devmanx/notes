41e51d3f761ed50f18bc987fe3efe58a860dd4cc
devmanx <devmanx@users.noreply.github.com>
2026-02-06 20:07:13 +0100

    Add Material Components dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material)
 
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.coroutines.android)
